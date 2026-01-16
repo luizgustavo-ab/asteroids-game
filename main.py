989e93a4e41837f8c0edda99fdf3ee5126f970b9
luizgustavo-ab <luizgustavoalvesbarros@gmail.com>
2026-01-16 16:13:14 -0300

    adding classes and movement

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
+
+    
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
-    print("Starting Asteroids with pygame version: VERSION")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
